eb9d764126b4f5d5636d010ad7cfe98b29c18c21
unknown <ashrithapuradamane@gmail.com>
2020-09-11 23:15:16 -0500

    Updates

---
+++ precache-manifest.9b415a10489fa715d16f2381a3a28dc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b10642e39016cbc91c05fb1a59d4b44",
+    "url": "/ashrithapb/React-Js-Job-portal-page.git/index.html"
+  },
+  {
+    "revision": "796180c8427db27e718e",
+    "url": "/ashrithapb/React-Js-Job-portal-page.git/static/css/main.2273eb1e.chunk.css"
+  },
+  {
+    "revision": "90aac834147ec266f39f",
+    "url": "/ashrithapb/React-Js-Job-portal-page.git/static/js/2.82bd41c6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ashrithapb/React-Js-Job-portal-page.git/static/js/2.82bd41c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "796180c8427db27e718e",
+    "url": "/ashrithapb/React-Js-Job-portal-page.git/static/js/main.7be0ec34.chunk.js"
+  },
+  {
+    "revision": "144be4a2f0e7a41af3c6",
+    "url": "/ashrithapb/React-Js-Job-portal-page.git/static/js/runtime-main.a7f78594.js"
+  }
+]);
